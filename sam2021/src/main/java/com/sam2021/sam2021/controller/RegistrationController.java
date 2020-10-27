081ff4c5afd659b093e48ccce6e0b9571cd87d7d
dpl2047 <dpl2047@g.rit.edu>
2020-10-26 23:55:14 -0700

    created the user model, adjusted registration html to thymeleaf, added model param

--- sam2021/src/main/java/com/sam2021/sam2021/controller/RegistrationController.java
+++ sam2021/src/main/java/com/sam2021/sam2021/controller/RegistrationController.java
@@ -1,5 +1,7 @@
 package com.sam2021.sam2021.controller;
 
+import com.sam2021.sam2021.models.User;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class RegistrationController {
     @RequestMapping(value = "/registration", method = RequestMethod.GET)
-    public String displayregister(){
+    public String displayregister(User user){
         return "registration";
     }
 }
--- sam2021/src/main/java/com/sam2021/sam2021/models/User.java
+++ sam2021/src/main/java/com/sam2021/sam2021/models/User.java
@@ -1,7 +1,5 @@
 package com.sam2021.sam2021.models;
 
-import javax.security.auth.login.AccountExpiredException;
-
 public class User {
     private String ftname;
     private String ltname;
@@ -9,10 +7,85 @@ public class User {
     private String phonenumber;
     private String affiliation;
     private String password;
+    private String confirmpassword;
     private AccountType accountType;
 
-    public User(String ftname, String ltname, String email, String phonenumber, String affiliation, String password, AccountType accountType){
-        
+    public User(String ftname, String ltname, String email, String phonenumber, String affiliation, String password, String confirmpassword,
+            AccountType accountType) {
+        this.ftname = ftname;
+        this.ltname = ltname;
+        this.email = email;
+        this.phonenumber = phonenumber;
+        this.affiliation = affiliation;
+        this.password = password;
+        this.confirmpassword = confirmpassword;
+        this.accountType = accountType;
+    }
+
+    //Getters
+    public String getFtname() {
+        return ftname;
+    }
+
+    public String getLtname() {
+        return ltname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public String getAffiliation() {
+        return affiliation;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getConfirmpassword() {
+        return confirmpassword;
+    }
+
+    public AccountType getAccountType() {
+        return accountType;
+    }
+
+    //Setters
+    public void setFtname(String ftname) {
+        this.ftname = ftname;
+    }
+
+    public void setLtname(String ltname) {
+        this.ltname = ltname;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public void setAffiliation(String affiliation) {
+        this.affiliation = affiliation;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setConfirmpassword(String confirmpassword) {
+        this.confirmpassword = confirmpassword;
+    }
+
+    public void setAccountType(AccountType accountType) {
+        this.accountType = accountType;
     }
 }
 
