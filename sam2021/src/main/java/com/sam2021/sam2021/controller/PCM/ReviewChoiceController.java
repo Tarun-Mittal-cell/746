a32d7c00124a45b3104517b2a1c0ad1dfdbc725e
kdd9198 <kyle200000@aol.com>
2020-11-15 23:14:06 -0500

    another commit before merging admin

--- sam2021/src/main/java/com/sam2021/sam2021/controller/PCM/ReviewChoiceController.java
+++ sam2021/src/main/java/com/sam2021/sam2021/controller/PCM/ReviewChoiceController.java
@@ -1,13 +1,30 @@
 package com.sam2021.sam2021.controller.PCM;
 
+import com.sam2021.sam2021.models.User;
+import com.sam2021.sam2021.service.PaperService;
+import com.sam2021.sam2021.service.TopicService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ReviewChoiceController {
+    @Autowired
+    private PaperService paperService;
+
+    @Autowired
+    private TopicService topicService;
+
     @RequestMapping(value="/HomepagePCM", method = RequestMethod.GET)
-    public String displayHomepage(){
+    public String displayHomepage(Model model){
+        User user = new User();
+        model.addAttribute("user", user);
+
+        //model.addAttribute("topics", topicService.getTopics());
+
         return "HomepagePCM";
     }
 }
