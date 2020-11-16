27b44d69a63203fa9af7bd7432f0a2842f0b8c91
kdd9198 <kyle200000@aol.com>
2020-11-15 23:13:34 -0500

    commit before merging in admin

---
+++ sam2021/src/main/java/com/sam2021/sam2021/service/PaperService.java
@@ -0,0 +1,5 @@
+package com.sam2021.sam2021.service;
+
+public class PaperService {
+    
+}
---
+++ sam2021/src/main/java/com/sam2021/sam2021/service/TopicService.java
@@ -0,0 +1,9 @@
+package com.sam2021.sam2021.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class TopicService {
+    
+    //@Autowired
+    //private TopicRepo topicRepo;
+}
