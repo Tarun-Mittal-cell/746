5ca2ed48b1f0baec50596490681841da4901d0c2
kdd9198 <kyle200000@aol.com>
2020-11-19 22:43:26 -0500

    read x num of text boxes on review submit

--- sam2021/src/main/java/com/sam2021/sam2021/controller/PCM/PCMReviewController.java
+++ sam2021/src/main/java/com/sam2021/sam2021/controller/PCM/PCMReviewController.java
@@ -15,9 +15,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PCMReviewController {
@@ -48,16 +50,34 @@ public class PCMReviewController {
 
     @GetMapping(value = "/PCMReview/select/{paperId}")
     public String redirectReviewPage(@PathVariable("paperId") long paperId, Model model){
-        return "redirect:/PCMReview/"+user.getId()+"/review/"+paperId;
+        return "redirect:/PCMReview/review/"+paperId;
     }
 
-    @RequestMapping(value = "/PCMReview/{userId}/review/{paperId}", method = RequestMethod.GET)
-    public String displayReviewPage(@PathVariable("userId") long userId, @PathVariable("paperId") long paperId, Model model){
+    @RequestMapping(value = "/PCMReview/review/{paperId}", method = RequestMethod.GET)
+    public String displayReviewPage(@PathVariable("paperId") long paperId, Model model){
         Paper paper = paperService.findById(paperId);
         String reviewTemplateName = paper.getTopic().getReviewTemp().getFilename();
 
         model.addAttribute("reviewTemplate", reviewTemplateName);
 
         return "ReviewContainer";
+    }
+
+    @RequestMapping(value = "/PCMReview/SubmitReview", method = RequestMethod.POST)
+    public String submitReview(@ModelAttribute("reviewContent") ReviewContent reviewContent){
+        
+        return "redirect:/PCMReview/"+user.getId();
+    }
+
+    public class ReviewContent{
+        public List<String> text;
+
+        public List<String> getText(){
+            return text;
+        }
+
+        public void setText(List<String> text){
+            this.text = text;
+        }
     }
 }
